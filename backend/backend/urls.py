a131383ee3ebeae55dd684dc41db4a816aa21f84
segupavankumar <62418622+segupavankumar@users.noreply.github.com>
2022-03-09 10:37:42 +0530

    removing verification

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -35,10 +35,10 @@ urlpatterns = [
     path('doctor/',include('doctor.urls')),
     path('articles/',include('article.urls')),
     path('accounts',include('allauth.urls')),
-    path('reset_password/',auth.PasswordResetView.as_view(), name='password_change'),
-    path('reset_password_sent/',auth.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',auth.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset_password_complete/',auth.PasswordResetCompleteView.as_view(), name='password_reset_complete')
+#     path('reset_password/',auth.PasswordResetView.as_view(), name='password_change'),
+#     path('reset_password_sent/',auth.PasswordResetDoneView.as_view(), name='password_reset_done'),
+#     path('reset/<uidb64>/<token>/',auth.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+#     path('reset_password_complete/',auth.PasswordResetCompleteView.as_view(), name='password_reset_complete')
 
 ]
 
